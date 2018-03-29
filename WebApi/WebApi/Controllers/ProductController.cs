a758ffc591ce65aea84b65fc21a8a635ead5272b
1035406110 <30611786+1035406110@users.noreply.github.com>
2018-03-29 15:17:29 +0800

    修改并完成实时产品接口

--- WebApi/WebApi/Controllers/ProductController.cs
+++ WebApi/WebApi/Controllers/ProductController.cs
@@ -11,11 +11,6 @@ namespace WebApi.Controllers
 {
     public class ProductController : ApiController
     {
-        [System.Web.Http.HttpGet]
-        public string test()
-        {
-            return "ASDadasasfasdfaswdf";
-        }
         /// <summary>
         /// 获取省级实时产品数据
         /// </summary>
@@ -25,12 +20,26 @@ namespace WebApi.Controllers
         /// <param name="diseaseType">病害类型编号</param>
         /// <param name="provId">省份编号</param>
         /// <returns>DataTable</returns>
-        [System.Web.Http.HttpGet]
-        public DataTable GetProvRealTimeProduct(DateTime date, int productType, int cropType = -1, int diseaseType = -1,int provId = -1)
+        [HttpGet]
+        public DataTable GetProvRealTimeProduct(DateTime date, int productType, int cropType = -1, int diseaseType = -1, int provId = -1)
         {
-            //string str = "";
-
-            string str = string.Format("select \"provid\",\"provname\",\"geom\",\"Value\" from public.\"Product_Realtime_Province\" join \"geom_province\" on \"geom_province\".\"provid\" = \"Product_Realtime_Province\".\"ProvinceId\" where \"Time\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} and \"ProvinceId\" = {4}", date, productType, cropType, diseaseType, provId);
+            string str=null;
+            //if (cropType == -1)
+            //{
+            //    cropType = 0;
+            //}
+            //if (diseaseType == -1)
+            //{
+            //    diseaseType = 0;
+            //}
+            //if (provId == -1)
+            //{
+            //    str = string.Format("select \"provid\",\"provname\",\"geom\",\"ProductValue\" from public.\"Product_Realtime_Province\" join \"geom_province\" on \"geom_province\".\"provid\" = \"Product_Realtime_Province\".\"ProvinceId\" where \"ProductDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} ", date, productType, cropType, diseaseType);
+            //}
+            //else
+            {
+                str = string.Format("select \"provid\",\"provname\",\"geom\",\"ProductValue\" from public.\"Product_Realtime_Province\" join \"geom_province\" on \"geom_province\".\"provid\" = \"Product_Realtime_Province\".\"ProvinceId\" where \"ProductDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} and \"ProvinceId\" = {4}", date, productType, cropType, diseaseType, provId);
+            }
             string tojson = string.Format(@"SELECT
 	                                jsonb_build_object (
 		                                'type',
@@ -65,13 +74,51 @@ namespace WebApi.Controllers
         /// <param name="diseaseType">病害类型编号</param>
         /// <param name="provId">地市编号</param>
         /// <returns>DataTable</returns>
-        [System.Web.Http.HttpGet]
+        [HttpGet]
         public DataTable GetCityRealTimeProduct(DateTime date, int productType, int cropType = -1, int diseaseType = -1, int cityId = -1)
         {
 
-
-            string str = "select * from public.\"Product_Realtime_City\"where productType = (@ProductTypeId) and cropType = (@CropTypeId) and diseaseType = (@DiseaseTypeId) and  CityId = (@cityId);";
-            return SunGolden.DBUtils.DbHelperPostgresql.ExecuteQuery(str, 1000).Tables[0];
+            string str = null;
+            if (cropType == -1)
+            {
+                cropType = 0;
+            }
+            if (diseaseType == -1)
+            {
+                diseaseType = 0;
+            }
+            if (cityId == -1)
+            {
+                str = string.Format("select \"cityid\",\"cityname\",\"geom\",\"ProductValue\" from public.\"Product_Realtime_City\" join \"geom_city\" on \"geom_city\".\"cityid\" = \"Product_Realtime_City\".\"CityId\" where \"ProductDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} ", date, productType, cropType, diseaseType);
+            }
+            else
+            {
+                str = string.Format("select \"cityid\",\"cityname\",\"geom\",\"ProductValue\" from public.\"Product_Realtime_City\" join \"geom_city\" on \"geom_city\".\"cityid\" = \"Product_Realtime_City\".\"CityId\" where \"ProductDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} and \"CityId\" = {4}", date, productType, cropType, diseaseType, cityId);
+            }
+            string tojson = string.Format(@"SELECT
+	                                jsonb_build_object (
+		                                'type',
+		                                'FeatureCollection',
+		                                'features',
+		                                jsonb_agg (feature)
+	                                )
+                                FROM
+	                                (
+		                                SELECT
+			                                jsonb_build_object (
+				                                'type',
+				                                'Feature',
+				                                'id',
+				                                cityid,
+				                                'geometry',
+				                                ST_AsGeoJSON (geom) :: jsonb,
+				                                'properties',
+				                                to_jsonb (ROW) - 'geom'
+			                                ) AS feature
+		                                FROM
+			                                ({0}) ROW
+	                                ) features;", str);
+            return SunGolden.DBUtils.DbHelperPostgresql.ExecuteQuery(tojson, 1000).Tables[0];
         }
         /// <summary>
         /// 获取县级实时产品数据
@@ -82,13 +129,52 @@ namespace WebApi.Controllers
         /// <param name="diseaseType">病害类型编号</param>
         /// <param name="provId">区县编号</param>
         /// <returns>DataTable</returns>
-        [System.Web.Http.HttpGet]
+        [HttpGet]
         public DataTable GetCountyRealTimeProduct(DateTime date, int productType, int cropType = -1, int diseaseType = -1, int countyId = -1)
         {
-
-            string str = "select * from public.\"Product_Realtime_County\" where productType = (@ProductTypeId) and cropType = (@CropTypeId) and diseaseType = (@DiseaseTypeId) and CountyId = (@countyId);";
-            return SunGolden.DBUtils.DbHelperPostgresql.ExecuteQuery(str, 1000).Tables[0];
+            string str = null;
+            if (cropType == -1)
+            {
+                cropType = 0;
+            }
+            if (diseaseType == -1)
+            {
+                diseaseType = 0;
+            }
+            if (countyId == -1)
+            {
+                str = string.Format("select \"counid\",\"counname\",\"geom\",\"ProductValue\" from public.\"Product_Realtime_County\" join \"geom_county\" on \"geom_county\".\"counid\" = \"Product_Realtime_County\".\"CountyId\" where \"ProductDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} ", date, productType, cropType, diseaseType);
+            }
+            else
+            {
+                str = string.Format("select \"counid\",\"counname\",\"geom\",\"ProductValue\" from public.\"Product_Realtime_County\" join \"geom_county\" on \"geom_county\".\"counid\" = \"Product_Realtime_County\".\"CountyId\" where \"ProductDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} and \"CountyId\" = {4}", date, productType, cropType, diseaseType, countyId);
+            }
+            string tojson = string.Format(@"SELECT
+	                                jsonb_build_object (
+		                                'type',
+		                                'FeatureCollection',
+		                                'features',
+		                                jsonb_agg (feature)
+	                                )
+                                FROM
+	                                (
+		                                SELECT
+			                                jsonb_build_object (
+				                                'type',
+				                                'Feature',
+				                                'id',
+				                                counid,
+				                                'geometry',
+				                                ST_AsGeoJSON (geom) :: jsonb,
+				                                'properties',
+				                                to_jsonb (ROW) - 'geom'
+			                                ) AS feature
+		                                FROM
+			                                ({0}) ROW
+	                                ) features;", str);
+            return SunGolden.DBUtils.DbHelperPostgresql.ExecuteQuery(tojson, 1000).Tables[0];
         }
+
         /// <summary>
         /// 获取全国实时空间分布产品数据
         /// </summary>
@@ -97,12 +183,21 @@ namespace WebApi.Controllers
         /// <param name="cropType">作物类型编号</param>
         /// <param name="diseaseType">病害类型编号</param>
         /// <returns>DataTable</returns>
-        [System.Web.Http.HttpGet]
+        [HttpGet]
         public DataTable GetRealTimeDistribution(DateTime date, int productType, int cropType = -1, int diseaseType = -1)
         {
-
-            string str = "select * from public.\"Product_Realtime_Distribution\"where productType = (@ProductTypeId) and cropType = (@CropTypeId) and diseaseType = (@DiseaseTypeId);";
+            
+            if (cropType == -1)
+            {
+                cropType = 0;
+            }
+            if (diseaseType == -1)
+            {
+                diseaseType = 0;
+            }
+            string str = string.Format("select \"Id\",\"GeoString\" from public.\"Product_RealTime_Distribution\"  where \"DistributionDate\" = '{0}' and \"ProductTypeId\" = {1} and \"CropTypeId\" = {2} and \"DiseaseTypeId\"  = {3} ", date, productType, cropType, diseaseType);
             return SunGolden.DBUtils.DbHelperPostgresql.ExecuteQuery(str, 1000).Tables[0];
+
         }
     }
 }
