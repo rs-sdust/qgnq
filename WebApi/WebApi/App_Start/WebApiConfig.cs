e8bbef49d119d868059ed270b81b84cede2a13de
1035406110 <30611786+1035406110@users.noreply.github.com>
2018-03-29 15:11:08 +0800

    自定义 "Product"、"Trend"两个路由。

--- WebApi/WebApi/App_Start/WebApiConfig.cs
+++ WebApi/WebApi/App_Start/WebApiConfig.cs
@@ -10,17 +10,25 @@ namespace WebApi
     {
         public static void Register(HttpConfiguration config)
         {
+            //Dic
             config.Routes.MapHttpRoute(
             name: "Dic",
             routeTemplate: "{controller}/{action}/",
             defaults: new { controller = "Dic", action = "GetProductType"}
-        );
-
+            );
+            //Product
             config.Routes.MapHttpRoute(
                 name: "Product",
                 routeTemplate: "{controller}/{action}/{date}/{productType}/{cropType}/{diseaseType}/{provId}",
-                defaults: new { controller = "Product", action = "GetProvRealTimeProduct", date = DateTime.Now, productType = 0, cropType = 0, diseaseType = UrlParameter.Optional, provId = UrlParameter .Optional}
+                defaults: new { controller = "Product", action = "GetProvRealTimeProduct", date = DateTime.Now, productType = 0, cropType = 0, diseaseType =UrlParameter.Optional, provId = UrlParameter.Optional }
             );
+            //config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+               name: "Trend",
+               routeTemplate: "{controller}/{action}/{start}/{days}/{productType}/{RegionId}/{cropType}/{diseaseType}",
+               defaults: new { controller = "Trend", action = "GetProvTrend", start = DateTime.Now, days = 1, productType = 0, RegionId = 0, cropType = UrlParameter.Optional, diseaseType = UrlParameter.Optional }
+            );
+          
         }
     }
 }
