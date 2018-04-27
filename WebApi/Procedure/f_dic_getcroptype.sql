983312d9c2b174c701283ebc1a5e351174182f08
yxq <bayberryan@hotmail.com>
2018-04-27 10:30:35 +0800

    clear all

--- WebApi/Procedure/f_dic_getcroptype.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION f_dic_getcroptype()
-RETURNS TABLE (id int,name varchar) 
-AS $BODY$
-BEGIN
-    RETURN QUERY 
-	SELECT * 
-	FROM dic_croptype; 
-END;
-$BODY$ LANGUAGE plpgsql;
--- WebApi/Procedure/f_dic_getprodtype.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION f_dic_getprodtype()
-RETURNS TABLE (id int,name varchar)
-AS $BODY$
-BEGIN
-    RETURN QUERY 
-	SELECT * 
-	FROM dic_prodtype; 
-END;
-$BODY$ LANGUAGE plpgsql;
